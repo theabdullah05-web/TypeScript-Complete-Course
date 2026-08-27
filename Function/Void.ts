41cee880bda1e77a34667c79a75853d598edc26f
Abdullah <abdullahbhtty01@gmail.com>
2026-08-27 16:14:20 +0500

    commit

--- Function/Void.ts
+++ Function/Void.ts
@@ -2,4 +2,4 @@
 function consoling(supHero: string = "Shaktimaan"): void {
   console.log(`My Favorite superhero is ${supHero}`);
 }
-consoling();
+consoling("Spiderman Sorry Others Just Recency Bias");
