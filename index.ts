b910adf4c5010d79a6b5412ec23d304cdb70e510
Abdullah <abdullahbhtty01@gmail.com>
2026-08-27 15:14:55 +0500

    commit

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 //String
 let nameIs: string = "Abdullah";
 console.log(nameIs);
-//
+//Number
 let favNum: Number = 5;
 favNum = 69;
 console.log(favNum);
+//Boolean
+let boolVar: Boolean = false;
+boolVar = true;
+console.log(boolVar);
