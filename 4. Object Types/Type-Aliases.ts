7f7f288e679fc1e447925263c56a8f5f253fd267
Abdullah <abdullahbhtty01@gmail.com>
2026-08-28 17:45:17 +0500

    commit

--- 4. Object Types/Type-Aliases.ts
+++ 4. Object Types/Type-Aliases.ts
@@ -6,3 +6,5 @@ type User = {
 function printUserInfo(user: User) {
   return `Name:${user.name} Age:${user.age} Location:${user.location}`;
 }
+const res = printUserInfo({ name: "Abdullah", age: 21, location: "Alewali" });
+console.log(res);
