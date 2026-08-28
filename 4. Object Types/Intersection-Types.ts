fb9d36e32ab6737da5543ccfda51e056e15a5faf
Abdullah <abdullahbhtty01@gmail.com>
2026-08-28 18:15:54 +0500

    commit

--- 4. Object Types/Intersection-Types.ts
+++ 4. Object Types/Intersection-Types.ts
@@ -9,3 +9,12 @@ type accountInfo = {
 };
 
 type personDetails = userInfo & accountInfo;
+
+const me: personDetails = {
+  name: "Abdullah",
+  age: 21,
+  semester: 1,
+  email: "theabullah@gmail.com",
+  password: "superman",
+};
+console.log(me);
