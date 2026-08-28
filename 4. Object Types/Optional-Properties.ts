52c59f325034d2aadf970a5546ce33b4fcb17222
Abdullah <abdullahbhtty01@gmail.com>
2026-08-28 17:56:36 +0500

    commit

---
+++ 4. Object Types/Optional-Properties.ts
@@ -0,0 +1,11 @@
+type Person = {
+  name: string;
+  age: number;
+  location?: string;
+};
+const user: Person = {
+  name: "Abdullah",
+  age: 21,
+  location: "Sambrial",
+};
+console.log(Person);
