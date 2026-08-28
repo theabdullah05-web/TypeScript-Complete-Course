aad8f73fa7f46f82f77ddfa55a855b2e8c039232
Abdullah <abdullahbhtty01@gmail.com>
2026-08-28 18:58:27 +0500

    commit

--- 5. Union Types/Literal-Types.ts
+++ 5. Union Types/Literal-Types.ts
@@ -1,1 +1,10 @@
 let color: "red" | "green" | "blue";
+
+let num: 1 | 2 | 3;
+num = 3;
+
+let isTrue: true;
+isTrue = true;
+
+let password: "secretPassword" = "secretPassword";
+console.log(password);
