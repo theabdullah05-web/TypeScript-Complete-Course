3e855ddbd4cb299556012186b04d387af9c71f81
Abdullah <abdullahbhtty01@gmail.com>
2026-08-28 16:03:36 +0500

    commit

---
+++ 3. Array Types/Multidimensional_Arrays.ts
@@ -0,0 +1,16 @@
+const singleArr: number[] = [1, 2, 3, 4];
+const doubleArr: number[][] = [
+  [1, 2],
+  [3, 4],
+];
+const tripArr: number[][][] = [
+  [
+    [1, 2],
+    [3, 4],
+  ],
+  [
+    [5, 6],
+    [7, 8],
+  ],
+  [[9], [10]],
+];
