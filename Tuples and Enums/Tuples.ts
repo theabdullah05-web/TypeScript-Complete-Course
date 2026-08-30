3fcb74424662dd52ce1995c6685f1f848812e5fd
Abdullah <abdullahbhtty01@gmail.com>
2026-08-30 20:25:23 +0500

    commit

--- Tuples and Enums/Tuples.ts
+++ Tuples and Enums/Tuples.ts
@@ -1,3 +1,9 @@
 let myTup: [string, number, boolean];
 myTup = ["abdullah", 21, true];
 console.log(myTup[1]);
+
+//destructuring
+let tup2: [string, string] = ["Abdullah", "Hussain"];
+let [first, second] = tup2;
+console.log(first);
+console.log(second);
