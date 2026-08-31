d8b0b07b705c219fe5052e8ff568562d70b64756
Abdullah <abdullahbhtty01@gmail.com>
2026-08-31 17:30:12 +0500

    commit

---
+++ 8. Interfaces/Interface.ts
@@ -0,0 +1,11 @@
+interface Computer {
+  name: string;
+  ram: number;
+  hdd: number;
+}
+const computerExample: Computer = {
+  name: "i5",
+  ram: 8,
+  hdd: 100,
+};
+console.log(computerExample.name);
