f11969b099d98f8ef5357bb9e58929b26b2a5f3b
Abdullah <abdullahbhtty01@gmail.com>
2026-08-31 15:40:17 +0500

    commit

--- Tuples and Enums/Enum.ts
+++ Tuples and Enums/Enum.ts
@@ -1,8 +1,14 @@
-enum WeatherCondtion {
+enum WeatherCondtions {
   Sunny,
   Rainy,
   Snowy,
   Cloudy,
+}
+enum WeatherCondtion {
+  Sunny = "sunny",
+  Rainy = "rainy",
+  Snowy = "snowy",
+  Cloudy = "cloudy",
 }
 let weather = WeatherCondtion.Cloudy;
 console.log(weather);
