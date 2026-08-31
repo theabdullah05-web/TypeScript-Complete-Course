a49e490ab9d43f51540fff769b67937ca6f218e2
Abdullah <abdullahbhtty01@gmail.com>
2026-08-31 20:05:35 +0500

    commit

--- 9. Generics/first-generic.ts
+++ 9. Generics/first-generic.ts
@@ -1,6 +1,10 @@
 function printNum(val: number, defaultVal: number): [number, number] {
   return [val, defaultVal];
 }
-function printNum
+function printString(a: string, b: string): [string, string] {
+  return [a, b];
+}
 const num = printNum(10, 20);
 console.log(num);
+const str = printString("Abdullah", "Hussain");
+console.log(str);
