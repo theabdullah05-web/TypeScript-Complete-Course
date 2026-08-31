b59f79e7d7615c26b9b41923ae5b37dd61c14092
Abdullah <abdullahbhtty01@gmail.com>
2026-08-31 17:39:03 +0500

    commit

---
+++ 8. Interfaces/2. Interface for function.ts
@@ -0,0 +1,11 @@
+interface mathOperation {
+  (x: number, y: number): number;
+}
+const add: mathOperation = (x, y) => {
+  return x + y;
+};
+const subtract: mathOperation = (a, b) => {
+  return b - a;
+};
+console.log(add(13, 54));
+console.log(subtract(10, 20));
