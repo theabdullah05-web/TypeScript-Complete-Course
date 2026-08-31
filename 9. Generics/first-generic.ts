20baff8411ea9985e21350c7d827722b1b7c7187
Abdullah <abdullahbhtty01@gmail.com>
2026-08-31 20:06:37 +0500

    commit

--- 9. Generics/first-generic.ts
+++ 9. Generics/first-generic.ts
@@ -4,7 +4,11 @@ function printNum(val: number, defaultVal: number): [number, number] {
 function printString(a: string, b: string): [string, string] {
   return [a, b];
 }
+function printBool(a: boolean, b: boolean): [boolean, boolean] {
+  return [a, b];
+}
 const num = printNum(10, 20);
 console.log(num);
 const str = printString("Abdullah", "Hussain");
 console.log(str);
+const bool = printBool(true, false);
