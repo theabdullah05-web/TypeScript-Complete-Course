f248f75f43da75f45c5133e9310ee961b788c83f
Abdullah <abdullahbhtty01@gmail.com>
2026-08-31 17:53:21 +0500

    commit

--- 8. Interfaces/interface-with-methods.ts
+++ 8. Interfaces/interface-with-methods.ts
@@ -3,3 +3,21 @@ interface User {
   age: number;
   sayHello(): void;
 }
+function greet(user: User) {
+  console.log(`${user.name}'s age is ${user.age}`);
+  user.sayHello();
+}
+const John: User = {
+  name: "John",
+  age: 30,
+  sayHello() {
+    console.log("Hi, there");
+  },
+};
+const Abdullah: User = {
+  name: "Abdullah",
+  age: 21,
+  sayHello() {
+    console.log("On your left");
+  },
+};
