6c33f416578d0bdd63ac94e4e18a7e41abf22846
Abdullah <abdullahbhtty01@gmail.com>
2026-08-31 16:51:39 +0500

    commit

--- 7. Classes/access-modifiers.ts
+++ 7. Classes/access-modifiers.ts
@@ -5,7 +5,10 @@ class Person {
     this.first = first;
     this.last = last;
   }
+  getName(): string {
+    return `Last Name is ${this.last}`;
+  }
 }
 let p1 = new Person("John", "Doe");
 console.log(p1.first);
-console.log(p1.last);
+console.log(p1.getName());
