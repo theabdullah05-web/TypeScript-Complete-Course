740fef15fd2dff5f9c4e2bc9ace375933f0eec19
Abdullah <abdullahbhtty01@gmail.com>
2026-08-31 20:16:02 +0500

    commit

--- 9. Generics/first-generic.ts
+++ 9. Generics/first-generic.ts
@@ -1,15 +1,24 @@
-function printNum(val: number, defaultVal: number): [number, number] {
-  return [val, defaultVal];
-}
-function printString(a: string, b: string): [string, string] {
+//Normal Zindagi
+// function printNum(val: number, defaultVal: number): [number, number] {
+//   return [val, defaultVal];
+// }
+// function printString(a: string, b: string): [string, string] {
+//   return [a, b];
+// }
+// function printBool(a: boolean, b: boolean): [boolean, boolean] {
+//   return [a, b];
+// }
+// const num = printNum(10, 20);
+// console.log(num);
+// const str = printString("Abdullah", "Hussain");
+// console.log(str);
+// const bool = printBool(true, false);
+// console.log(bool);
+
+//Mentos Zindagi
+function MultiTypeGenFunc<T>(a: T, b: T): [T, T] {
   return [a, b];
 }
-function printBool(a: boolean, b: boolean): [boolean, boolean] {
-  return [a, b];
-}
-const num = printNum(10, 20);
-console.log(num);
-const str = printString("Abdullah", "Hussain");
-console.log(str);
-const bool = printBool(true, false);
-console.log(bool);
+let str = MultiTypeGenFunc<string>("Hello", "World");
+let num = MultiTypeGenFunc<number>(21, 26);
+let bool = MultiTypeGenFunc<boolean>(true, false);
