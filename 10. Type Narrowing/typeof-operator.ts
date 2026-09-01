15a20850fbfe504cf671b0d8923a0200343a52f7
Abdullah <abdullahbhtty01@gmail.com>
2026-09-01 17:19:58 +0500

    commit

---
+++ 10. Type Narrowing/typeof-operator.ts
@@ -0,0 +1,8 @@
+type MyType = string | number;
+function exampleFunction(a: MyType): void {
+  if (typeof a === "string") {
+    console.log(a.toUpperCase());
+  } else {
+    console.log(a.toFixed(2));
+  }
+}
