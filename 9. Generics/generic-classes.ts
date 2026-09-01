fe5d16b5d898a0c05ccf47f2a62eb50233da6013
Abdullah <abdullahbhtty01@gmail.com>
2026-09-01 16:41:49 +0500

    commit

--- 9. Generics/generic-classes.ts
+++ 9. Generics/generic-classes.ts
@@ -6,7 +6,11 @@ class Box<T> {
   get getContent() {
     return this.initialContent;
   }
-  set showContent(newContent: T) {
+  set editContent(newContent: T) {
     this.initialContent = newContent;
   }
 }
+let b1 = new Box<string>("Hello World");
+console.log(b1.getContent);
+b1.editContent = "TypeScript";
+console.log(b1.getContent);
