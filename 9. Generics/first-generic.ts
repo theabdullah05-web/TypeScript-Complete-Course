f237501b39ff93e6513a4f7392612f189ceab425
Abdullah <abdullahbhtty01@gmail.com>
2026-09-01 14:43:58 +0500

    commit

--- 9. Generics/first-generic.ts
+++ 9. Generics/first-generic.ts
@@ -1,58 +1,38 @@
 //Normal Function
-// function printNum(val: number, defaultVal: number): [number, number] {
-//   return [val, defaultVal];
-// }
-// function printString(a: string, b: string): [string, string] {
-//   return [a, b];
-// }
-// function printBool(a: boolean, b: boolean): [boolean, boolean] {
-//   return [a, b];
-// }
-// const num = printNum(10, 20);
-// console.log(num);
-// const str = printString("Abdullah", "Hussain");
-// console.log(str);
-// const bool = printBool(true, false);
-// console.log(bool);
+function printNum(val: number, defaultVal: number): [number, number] {
+  return [val, defaultVal];
+}
+function printString(a: string, b: string): [string, string] {
+  return [a, b];
+}
+function printBool(a: boolean, b: boolean): [boolean, boolean] {
+  return [a, b];
+}
+const num1 = printNum(10, 20);
+console.log(num1);
+const str1 = printString("Abdullah", "Hussain");
+console.log(str1);
+const bool1 = printBool(true, false);
+console.log(bool1);
 
 //Generics
-// function MultiTypeGenFunc<T>(a: T, b: T): [T, T] {
-//   return [a, b];
-// }
-// let str = MultiTypeGenFunc<string>("Hello", "World");
-// let num = MultiTypeGenFunc<number>(21, 26);
-// let bool = MultiTypeGenFunc<boolean>(true, false);
-// console.log(str);
-// console.log(num);
-// console.log(bool);
+function MultiTypeGenFunc<T>(a: T, b: T): [T, T] {
+  return [a, b];
+}
+let str = MultiTypeGenFunc<string>("Hello", "World");
+let num = MultiTypeGenFunc<number>(21, 26);
+let bool = MultiTypeGenFunc<boolean>(true, false);
+console.log(str);
+console.log(num);
+console.log(bool);
 
 //interface & generics
-// interface Dog {
-//   name: string;
-//   breed: string;
-// }
-// let dog1 = MultiTypeGenFunc<Dog>(
-//   { name: "Puppy", breed: "Some Breed" },
-//   { name: "Puppy2", breed: "Stray" },
-// );
-// console.log(dog1);
-
-//Another Example
-function getRandomKeyValuePair<T>(obj: { [key: string]: T }): {
-  key: string;
-  value: T;
-} {
-  const keys = Object.keys(obj);
-  const randKey = keys[Math.floor(Math.random() * keys.length)];
-  return {
-    key: randKey,
-    value: obj[randKey],
-  };
+interface Dog {
+  name: string;
+  breed: string;
 }
-const stringObject = { a: "Avacado", b: "Blueberry", c: "Cantaloupe" };
-let randVal = getRandomKeyValuePair<string>(stringObject);
-console.log(randVal);
-
-const numObject = { one: 1, two: 2, three: 3 };
-let randNum = getRandomKeyValuePair<number>(numObject);
-console.log(randNum);
+let dog1 = MultiTypeGenFunc<Dog>(
+  { name: "Puppy", breed: "Some Breed" },
+  { name: "Puppy2", breed: "Stray" },
+);
+console.log(dog1);
