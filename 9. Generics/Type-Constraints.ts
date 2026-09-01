b0c983adc42e1b2e0cd39909a4d2d70547b224fb
Abdullah <abdullahbhtty01@gmail.com>
2026-09-01 17:02:21 +0500

    commit

--- 9. Generics/Type-Constraints.ts
+++ 9. Generics/Type-Constraints.ts
@@ -7,3 +7,13 @@ function mergeObjects<T extends Identifier, U extends Record<string, any>>(
 ): T & U {
   return { ...obj1, ...obj2 };
 }
+let obj1 = {
+  id: 1,
+  name: "Abdullah",
+};
+let obj2 = {
+  id: 2,
+  age: 21,
+};
+const merged = mergeObjects(obj1, obj2);
+console.log(merged);
