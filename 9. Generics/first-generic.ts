e5d4661b997c1f985de6921467e8abe7642671ac
Abdullah <abdullahbhtty01@gmail.com>
2026-09-01 14:38:03 +0500

    commit

--- 9. Generics/first-generic.ts
+++ 9. Generics/first-generic.ts
@@ -36,3 +36,18 @@ let dog1 = MultiTypeGenFunc<Dog>(
   { name: "Puppy2", breed: "Stray" },
 );
 console.log(dog1);
+
+//Another Example
+function getRandomKeyValuePair<T>(obj: { [key: string]: T }): {
+  key: string;
+  value: T;
+} {
+  const keys = Object.keys(obj);
+  const randKey = keys[Math.floor(Math.random() * keys.length)];
+  return {
+    key: randKey,
+    value: obj[randKey],
+  };
+}
+const stringObject = { a: "Avacado", b: "Blueberry", c: "Cantaloupe" };
+getRandomKeyValuePair<string>(stringObject);
