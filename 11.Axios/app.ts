54ee3e4ad6e5cf76c810c699f95097624ae8822d
Abdullah <abdullahbhtty01@gmail.com>
2026-09-02 11:54:07 +0500

    commit

--- 11.Axios/app.ts
+++ 11.Axios/app.ts
@@ -1,1 +1,12 @@
-import axios from "axios";
+import axios, { Axios, type AxiosResponse } from "axios";
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+const fetchData = async () => {
+  const response: AxiosResponse<Todo> = await axios.get(
+    "https://jsonplaceholder.typicode.com/todos/1",
+  );
+};
