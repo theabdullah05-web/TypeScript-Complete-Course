3d0354ddb16951926a309d006d4b56a3dc38c9bf
Abdullah <abdullahbhtty01@gmail.com>
2026-09-02 11:57:08 +0500

    commit

--- 11.Axios/app.ts
+++ 11.Axios/app.ts
@@ -10,7 +10,7 @@ const fetchData = async () => {
     const response: AxiosResponse<Todo> = await axios.get(
       "https://jsonplaceholder.typicode.com/todos/1",
     );
-    console.log(response.data);
+    console.log("Todo:", response.data);
   } catch (error: any) {
     console.log("Error", error.message);
   }
